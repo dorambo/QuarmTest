6576494faab80bca38e8a31856e727ab20071bec
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-09-12 21:50:33 -0400

    Initialize evo patch in EQStreamIdentifier

--- common/patches/patches.cpp
+++ common/patches/patches.cpp
@@ -3,13 +3,16 @@
 #include "patches.h"
 
 #include "mac.h"
+#include "evo.h"
 
 void RegisterAllPatches(EQStreamIdentifier &into) {
 	Mac::Register(into);
+	Evo::Register(into);
 
 }
 
 void ReloadAllPatches() {
 	Mac::Reload();
+	Evo::Reload();
 }
 
