a5d359d264605d326974379f590a3e1be478e7c4
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-09-12 21:49:00 -0400

    Initial evo commit.

---
+++ common/patches/evo.h
@@ -0,0 +1,38 @@
+#ifndef EVO_H_
+#define EVO_H_
+
+#include "../struct_strategy.h"
+
+class EQStreamIdentifier;
+
+namespace Evo {
+
+	//these are the only public member of this namespace.
+	extern void Register(EQStreamIdentifier &into);
+	extern void Reload();
+
+
+
+	//you should not directly access anything below..
+	//I just dont feel like making a seperate header for it.
+
+	class Strategy : public StructStrategy {
+	public:
+		Strategy();
+
+	protected:
+
+		virtual std::string Describe() const;
+		virtual const EQ::versions::ClientVersion ClientVersion() const;
+		//magic macro to declare our opcodes
+		#include "ss_declare.h"
+		#include "evo_ops.h"
+
+
+	};
+
+};
+
+
+
+#endif /*EVO_H_*/
